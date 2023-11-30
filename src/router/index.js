52a5d12c55fc23427b21dde43ce979a43f3c7e5e
myt <381618248@qq.com>
2023-11-30 20:16:56 +0800

    perf:文案调整

--- src/router/index.js
+++ src/router/index.js
@@ -308,7 +308,7 @@ const routes = [
   {
     path: "/",
     component: main,
-    name: "身份认证",
+    name: "身份验证",
     nameKey: "PrivateKey",
     leaf: true,
     menuShow: true,
@@ -317,7 +317,7 @@ const routes = [
       {
         path: "/privateKeyManagement",
         component: privateKeyManagement,
-        name: "身份认证",
+        name: "身份验证",
         nameKey: "PrivateKey",
         menuShow: true,
         meta: { requireAuth: true }
